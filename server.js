848c10d7749344ae5cab25299a5725c2de930b63
patrick andrews <patrickxandrews@gmail.com>
2021-01-15 09:26:18 -0700

    build endpoints with local array storage

--- server.js
+++ server.js
@@ -10,14 +10,23 @@ app.use(express.urlencoded({ extended: true }))
 app.listen(3000, () => {
 })
 
+const contacts = [];
+
 app.get('/contacts', (req, res) => {
+    res.send(contacts);
 })
 
 app.post('/contacts', (req, res) => {
+    contacts.push(req.body);
+    res.status(200).send(req.body);
 })
 
 app.put('/contacts/:id', (req, res) => {
+    contacts[contacts.findIndex(val => val.id == req.params.id)] = req.body;
+    res.status(200).send(req.body);
 })
 
 app.delete('/contacts/:id', (req, res) => {
+    contacts.splice(contacts.findIndex(val => val.id === req.params.id), 1);
+    res.status(200).send(req.params.id);
 })
