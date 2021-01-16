bee318c58c4dca77ea648e2c4f98cee2b22caadd
patrick <patrickxandrews@gmail.com>
2021-01-16 09:54:42 -0700

    all endpoints connect to mongo. update readme

--- server.js
+++ server.js
@@ -15,29 +15,52 @@ app.use(express.urlencoded({ extended: true }))
 app.listen(3000, () => {
 })
 
-const contacts = [];
-
-app.get('/contacts', (req, res) => {
-    res.send(contacts.sort((a, b) => (a.name > b.name) ? 1 : -1));
+// default get all contacts sorted by name
+app.get('/contacts', async (req, res) => {
+    try {
+        const contacts = await contactModel.find().sort({name: "desc"});
+        res.status(200).send(contacts);
+    } catch {
+        res.status(500).send("Error fetching contacts!");
+    }
 })
 
-app.get('/contacts/:sortType/:sortDir', (req, res) => {
-    res.send(req.params.sortDir === 'desc' ? 
-    contacts.sort((a, b) => (a[req.params.sortType] > b[req.params.sortType]) ? 1 : -1)
-    : contacts.sort((a, b) => (a[req.params.sortType] < b[req.params.sortType]) ? 1 : -1));
+// get all contacts sorted by 'type' and 'direction'
+app.get('/contacts/:sortType/:sortDir', async (req, res) => {
+    try {
+        const contacts = await contactModel.find().sort({[req.params.sortType]: req.params.sortDir});
+        res.status(200).send(contacts);
+    } catch {
+        res.status(500).send("Error fetching contacts!");
+    }
 })
 
-app.post('/contacts', (req, res) => {
-    contacts.push(req.body);
-    res.status(200).send(req.body);
+// post a new contact
+app.post('/contacts', async (req, res) => {
+    try {
+        const contact = await new contactModel({ ...req.body }).save();
+        res.status(200).send(contact);
+    } catch {
+        res.status(500).send("Error creating contact.");
+    }
 })
 
-app.put('/contacts/:id', (req, res) => {
-    contacts[contacts.findIndex(val => val.id == req.params.id)] = req.body;
-    res.status(200).send(req.body);
+// update contact by id
+app.put('/contacts/:id', async (req, res) => {
+    try {
+        const contact = await contactModel.findOneAndUpdate({ id: req.params.id }, req.body, {useFindAndModify : false});
+        res.status(200).send(contact);
+    } catch {
+        res.status(404).send("Contact doesn't exist!");
+    }
 })
 
-app.delete('/contacts/:id', (req, res) => {
-    contacts.splice(contacts.findIndex(val => val.id == req.params.id), 1);
-    res.status(200).send(req.params.id);
+// delete contact by id
+app.delete('/contacts/:id', async (req, res) => {
+    try {
+        const contact = await contactModel.deleteOne({ id: req.params.id });
+        res.status(200).send(contact);
+    } catch {
+        res.status(404).send("Contact doesn't exist!");
+    }
 })
